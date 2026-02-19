ea87ee1cbbedc0f02dbe0a7293d593d73e1ba3be
Hemaang Sood <h3sood@uwaterloo.ca>
2026-02-18 23:16:28 -0500

    feat: implement global loaded_env variable to manage environment loading state

--- backend/src/backend/config/env_vars.py
+++ backend/src/backend/config/env_vars.py
@@ -7,7 +7,13 @@ from backend.loggers.logger import get_logger # type: ignore
 logger = get_logger(__name__)
 
 env_file_path = os.path.join(pathlib.Path(__file__).parent.parent.parent.parent.parent, 'env', '.env')
+
+loaded_env:bool = False
+
 def load_env_variables() -> bool:
+	global loaded_env
+	if loaded_env:
+		return loaded_env
 	logger.info(f"Attempting to load environment variables from {env_file_path}")
 	loaded_env = load_dotenv(env_file_path)
 	if not loaded_env:
