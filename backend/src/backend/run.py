6672e98128d4fae0bb54b1f181dfdd644aead8e2
Hemaang Sood <h3sood@uwaterloo.ca>
2026-02-18 19:37:22 -0500

    feat: add poetry install command to dev function for dependency management

--- backend/src/backend/run.py
+++ backend/src/backend/run.py
@@ -1,7 +1,10 @@
+import subprocess
+import sys
 import uvicorn
 
 def dev():
-    uvicorn.run(
+	subprocess.run(["poetry", "install"],check=True)
+	uvicorn.run(
         "backend.main:app",
         host="0.0.0.0",
         port=8000,
