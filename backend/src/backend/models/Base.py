f74d82c1c85f516f3b0baff76269a2125264bce2
Hemaang Sood <h3sood@uwaterloo.ca>
2026-02-18 21:46:43 -0500

    feat: update environment variable handling and dependencies for improved configuration, also enabled usage of non default schema, ready for supabase  usage

--- backend/src/backend/models/Base.py
+++ backend/src/backend/models/Base.py
@@ -1,4 +1,13 @@
+from sqlalchemy import MetaData
 from sqlalchemy.orm import DeclarativeBase
+import os
+from backend.config.env_vars import load_env_variables
+from backend.loggers.logger import get_logger # type: ignore
+
+loaded_env = load_env_variables()
+logger = get_logger(__name__)
+
+logger.info("Initializing Base model with schema: %s", os.getenv('DB_SCHEMA', 'Not set'))
 
 class Base(DeclarativeBase):
-	pass
+	metadata = MetaData(schema=os.getenv('DB_SCHEMA', 'public'))
